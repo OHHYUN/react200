b2951ad4175dfee9c8ad8ecde4154a6a2c10305d
OHHYUN <omplcie@gmail.com>
2021-08-21 23:39:23 +0900

    add: arrowFunc

---
+++ src/R013_ArrowFunction.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class R013_ArrowFunction extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrowFuc: 'React200',
+            num: 3
+        };
+    }
+
+    componentDidMount() {
+        Function1(1);
+        function Function1(num1) {
+            return console.log(num1 + '. Es5 Function');
+        }
+
+        Function2 = (num1, num2) => {
+            let num3 = num1 + num2;
+            console.log(num3 + ' . Arrow Function : ' + this.state.arrowFuc);
+        }
+
+        Function3() {
+            var this_bind = this;
+            setTimeout(function () {
+                console.log(this_bind.state.num + '. Es5 Callback function noBind');
+            })
+        }
+
+    }
+
+    render() {
+        return (
+            <h2> [THIS IS ARROWFUNCTION] </h2>
+        )
+    }
+}
+
+export default R013_ArrowFunction
