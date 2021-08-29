46543fdba8c298aeece4b9ba51929c0521279f66
OHHYUN <omplcie@gmail.com>
2021-08-29 19:33:47 +0900

    add: Props Object

---
+++ src/R020_PropsObjVal.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import datatype from 'prop-types';
+
+class R020_PropsObjVal extends Component {
+    render() {
+        let {
+            ObjectJson
+        } = this.props
+        return (
+            <div>
+                {JSON.stringify(ObjectJson)}
+            </div>
+        )
+    }
+}
+
+R020_PropsObjVal.proTypes = {
+    ObjectJson: datatype.shape({
+        react: datatype.string,
+        twohundred: datatype.number
+    })
+}
+export default R020_PropsObjVal
