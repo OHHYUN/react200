a8bef8b9ad145cd0e996ae9dc37b75c04c7c1066
OHHYUN <omplcie@gmail.com>
2021-09-03 23:21:25 +0900

    add: PropsNode

---
+++ src/R023_PropsNode.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class R023_PropsNode extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+export default R023_PropsNode
