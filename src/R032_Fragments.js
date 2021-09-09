2e0ec48813263891d32c3e773170358ea53be40c
OHHYUN <omplcie@gmail.com>
2021-09-09 23:27:18 +0900

    add: Fragments

---
+++ src/R032_Fragments.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class R032_Fragments extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>P tag</p>
+                <span>SPAN TAG</span>
+            </React.Fragment>
+        )
+    }
+
+}
+
+export default R032_Fragments
